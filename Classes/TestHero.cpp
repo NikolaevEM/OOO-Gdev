64fd03404e26facff09c91deeee873532141f370
NikolaevEM <43157756+NikolaevEM@users.noreply.github.com>
2021-04-17 14:48:41 +0900

    Add files via upload

---
+++ Classes/TestHero.cpp
@@ -0,0 +1,9 @@
+#include "TestHero.h"
+#include <iostream>
+ //include сцену игры
+
+USING_NS_CC;
+
+TestHero::TestHero() {
+	
+}
---
+++ Classes/TestHero.h
@@ -0,0 +1,12 @@
+#ifndef __TEST_HERO_H__
+#define __TEST_HERO_H__
+
+#include "cocos2d.h"
+
+class TestHero : public cocos2d::Scene
+{
+public:
+	TestHero();
+};
+
+#endif // __TEST_SCENE_H__#pragma once
