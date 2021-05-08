b186d33eb3d0d2092ed0c02bf219e0ca10ea3c20
uiop49 <43157727+uiop49@users.noreply.github.com>
2021-05-08 13:35:26 +0900

    Add files via upload

---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    public int damage = 1;
+    public bool isEnemyShot = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // timer to die
+        Destroy(gameObject, 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
