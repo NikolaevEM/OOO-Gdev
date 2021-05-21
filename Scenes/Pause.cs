3e81b911c7067579559c4ca63770c2e231ec1bc2
Mikhail Petrov <43157772+petrovmm@users.noreply.github.com>
2021-05-22 00:40:58 +0900

    Локализация ENG+RUS

---
+++ Scenes/Pause.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool paused = false;
+    public GameObject Panel;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!paused)
+            {
+                pausedMenu();
+            } else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        paused = false;
+        Panel.SetActive(false);
+    }
+    
+    void pausedMenu()
+    {
+        Time.timeScale = 0;
+        paused = true;
+        Panel.SetActive(true);
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Scripts/SwitchLanguage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLanguage : MonoBehaviour
+{
+    public void Switch(int _scene)
+    {
+        SceneManager.LoadScene(_scene);
+    }
+}
