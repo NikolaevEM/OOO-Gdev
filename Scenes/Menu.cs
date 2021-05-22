fbf6374a4d783731df2f4ce030b6c867564bbe6e
NikolaevEM <43157756+NikolaevEM@users.noreply.github.com>
2021-05-22 12:30:39 +0900

    Merge 1.02

---
+++ Scenes/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject Continue;
+
+    public string sceneToLoad = "SampleScene";
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ContinueButton()
+    {
+        MainMenu.SetActive(false);
+        Continue.SetActive(true);
+    }
+    public void ExitMenuButton()
+    {
+        MainMenu.SetActive(true);
+        Continue.SetActive(false);
+    }
+}
