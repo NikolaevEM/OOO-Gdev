3d3c3131beafa8ad27c4312c2b583479c9f4b5bd
NikolaevEM <43157756+NikolaevEM@users.noreply.github.com>
2021-05-22 12:33:23 +0900

    Merge 1.02
    Spawn not working
    Pause menu bug

---
+++ Cursor2/Assets/Scripts/Ammo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    // Актуальный скрипт боеприпаса
+
+    public float speed;
+    public float destroyTime;
+
+    void Start()
+    {
+        Invoke("DestroyAmmo", destroyTime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    void DestroyAmmo()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Cursor2/Assets/Scripts/ShootingObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingObject : MonoBehaviour
+{
+    // Актуальный скрипт спавна ammo по направлению по КД
+
+    public float offset;
+
+    public GameObject ammo;
+    public Transform shotDir;
+
+    private float timeShot;
+    public float startTime;
+
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotateZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + offset);
+
+        if (timeShot <= 0)
+        {
+            Instantiate(ammo, shotDir.position, transform.rotation);
+            timeShot = startTime;
+        }
+        else
+        {
+            timeShot -= Time.deltaTime;
+        }
+    }
+}
