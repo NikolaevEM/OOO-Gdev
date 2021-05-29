a2e5ce821ec0578c1370fc638d387093ffeb82c1
Mikhail Petrov <43157772+petrovmm@users.noreply.github.com>
2021-05-29 10:43:45 +0900

    Add files via upload

---
+++ falldeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falldeath : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -7f )//|| collision.gameObject.tag == "Crack")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
