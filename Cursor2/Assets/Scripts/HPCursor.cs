562f569c864f3d67825e132e5e1b1992b8314f3c
NikolaevEM <43157756+NikolaevEM@users.noreply.github.com>
2021-06-02 13:48:45 +0900

    Merge 1.2

---
+++ Cursor2/Assets/Scripts/HPCursor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPCursor : MonoBehaviour
+{
+    // Актуальный скрипт
+
+    public int health;
+    public int numberOfLives;
+
+    public Image[] lives;
+
+    public Sprite HP1;
+    public Sprite HP0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+
